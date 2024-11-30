5367d4fbae21b9a90c97de2894f2934d5533fab8
Jeron Aldaron Lau <aldaronlau@gmail.com>
2024-11-30 02:38:32 -0600

    Obfuscate generics in bounds

--- src/common/obfuscate_generics.rs
+++ src/common/obfuscate_generics.rs
@@ -1,23 +1,57 @@
-use syn::{GenericParam, Generics, Ident};
+use syn::{
+    punctuated::Punctuated, token::Comma, CapturedParam, GenericParam,
+    Generics, Ident, TypeParamBound,
+};
 
 /// Obfuscate user-defined generics, so they don't conflict with traitful's
 /// additional generics.
 pub(crate) fn obfuscate_generics(mut generics: Generics) -> Generics {
-    for param in generics.params.iter_mut() {
+    obfuscate_params(&mut generics.params);
+    generics
+}
+
+/// Obfuscate the generic parameters.
+fn obfuscate_params(params: &mut Punctuated<GenericParam, Comma>) {
+    for param in params.iter_mut() {
         match param {
             GenericParam::Lifetime(ref mut param) => {
                 obfuscate_ident(&mut param.lifetime.ident);
             }
             GenericParam::Type(ref mut param) => {
                 obfuscate_ident(&mut param.ident);
+
+                for bound in param.bounds.iter_mut() {
+                    match bound {
+                        TypeParamBound::Trait(ref mut bound) => {
+                            if let Some(ref mut lifetimes) = bound.lifetimes {
+                                obfuscate_params(&mut lifetimes.lifetimes);
+                            }
+                        }
+                        TypeParamBound::Lifetime(ref mut bound) => {
+                            obfuscate_ident(&mut bound.ident);
+                        }
+                        TypeParamBound::PreciseCapture(ref mut bound) => {
+                            for param in bound.params.iter_mut() {
+                                match param {
+                                    CapturedParam::Lifetime(ref mut param) => {
+                                        obfuscate_ident(&mut param.ident);
+                                    }
+                                    CapturedParam::Ident(ref mut param) => {
+                                        obfuscate_ident(param);
+                                    }
+                                    _ => {}
+                                }
+                            }
+                        }
+                        _ => {}
+                    }
+                }
             }
             GenericParam::Const(ref mut param) => {
                 obfuscate_ident(&mut param.ident);
             }
         }
     }
-
-    generics
 }
 
 /// Obfuscate an ident by adding a trailing '_'.
